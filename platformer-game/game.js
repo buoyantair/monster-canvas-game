f8aec5d8a779ee7b30cd0939850f86ff47bf5190
buoyantair <buoyantair@gmail.com>
2017-04-15 16:16:26 +0530

    Created simple platform with nothing else but a sky, ground and a player rect objects, player can move horizontally

--- platformer-game/game.js
+++ platformer-game/game.js
@@ -1,19 +1,79 @@
 // Create canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.height = 600;
 canvas.width = 700;
+canvas.height = 600;
 document.body.appendChild(canvas);
 
-function renderBackground() {
-  var img = new Image();
-  img.onload = function() {
-    var patrn = ctx.createPattern(img, "repeat");
-    ctx.fillStyle = patrn;
-    ctx.fillRect(0,0, canvas.width, canvas.height);
-  };
-  img.src = 'images/tiles/Backgrounds/blue_land.png';
+var player = {
+  x : 100,
+  y : 450,
+  speed: 10
+}
+
+function drawSky(){
+  ctx.fillStyle = '#8CDAFF'
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawLand(){
+  ctx.fillStyle = '#3E7057'
+  ctx.fillRect(0, 500, canvas.width, 100);
+}
+
+function drawPlayer(){
+  ctx.fillStyle = "#FFF"
+  ctx.fillRect(player.x, player.y, 50, 50);
 }
 
 
-renderBackground();
+var keysDown = {};
+
+addEventListener('keydown', function(e){
+  keysDown[e.keyCode] = true;
+}, false)
+
+addEventListener('keyup', function(e){
+  delete keysDown[e.keyCode]
+}, false)
+
+var jump = function(){
+  for(var i =0; i < 100; i++){
+    player.y--;
+  }
+
+}
+
+var update = function(){
+  if(38 in keysDown){
+    // Jump
+    jump();
+  }
+  if(37 in keysDown){
+    // Left
+    player.x -= player.speed;
+  }
+  if(39 in keysDown){
+    // Right
+    player.x += player.speed;
+  }
+
+  if(player.x > canvas.width){
+    player.x = 0;
+  } else if (player.x < 0){
+    player.x = canvas.width;
+  } 
+}
+
+var main = function(){
+  drawSky();
+  drawLand();
+  update();
+  drawPlayer();
+  requestAnimationFrame(main);
+}
+
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+main();
